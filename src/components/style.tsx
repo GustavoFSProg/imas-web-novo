ef974f6da763dba9784f5527bb90749226aa36c0
Gustavo <gustavo.prog40@gmail.com>
2021-03-30 15:57:15 -0300

    fazendo

--- src/components/style.tsx
+++ src/components/style.tsx
@@ -2,4 +2,9 @@ import styled from 'styled-components'
 
 export const CarouselContainer = styled.div`
   margin-top: -92px;
+
+  @media screen and (max-width: 600px) {
+    width: 128%;
+    margin-left: -30px;
+  }
 `
